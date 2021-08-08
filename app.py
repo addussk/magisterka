db9edc5719c4ee7c8bcc30020dcee2688a7b3df5
addussk <34662199+addussk@users.noreply.github.com>
2021-08-08 11:03:49 +0200

    refactoring app file

--- app.py
+++ app.py
@@ -3,25 +3,37 @@ from flask import Flask
 from scripts import dummy_temperature
 from dashApp.webapp import create_app
 
-app = Flask(__name__)
+from dashApp.models import Frequency, Temperature, Ustawienia
+app = Flask(__name__, instance_relative_config=False)
 
 app, db = create_app(app)
 
+def gen_dummy_freq():
+    # print("Measure frequency...")
+    db.session.add(Frequency(measured_freq=psutil.cpu_percent(), time_of_measurement=datetime.datetime.now()))
+    db.session.commit()
+
+def gen_dummy_temp():
+    db.session.add(Temperature(measured_temp=dummy_temperature(), time_of_measurement=datetime.datetime.now()))
+    db.session.commit()
+
+def read_setting_from_db():
+    latest_set = db.session.query(Ustawienia).order_by(Ustawienia.id.desc()).first()
+    return latest_set.get()
+
 def made_measurement():
-    from dashApp.models import Frequency, Temperature
     while(True):
-        print("Started task...")
-        print("%s: %s" % (threading.current_thread().name, time.ctime(time.time())))
+        # print("Started task...")
+        # print("%s: %s" % (threading.current_thread().name, time.ctime(time.time())))
+        curr_setting = read_setting_from_db()
+        print(curr_setting)
         time.sleep(10)
+        gen_dummy_freq()
 
-        print("Measure frequency...")
-        db.session.add(Frequency(measured_freq=psutil.cpu_percent(), time_of_measurement=datetime.datetime.now()))
-        db.session.commit()
-
-        print("Measure temperature...")
-        db.session.add(Temperature(measured_temp=dummy_temperature(), time_of_measurement=datetime.datetime.now()))
-        db.session.commit()
-        print("task completed")
+        # print("Measure temperature...")
+        gen_dummy_temp()
+        
+        # print("task completed")
 
 t2 = threading.Thread(target=made_measurement)
 t2.start()
@@ -29,6 +41,7 @@ t2.start()
 if __name__ == '__main__':
     db.create_all()
     app.run_server(debug=True)
+    t2.join()
     # t1 = threading.Thread(target=app.run_server)
     # t1.start()
     
