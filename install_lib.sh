6390ccc536b7c7c1d6e33db43b58f925228e3857
addussk <34662199+addussk@users.noreply.github.com>
2021-12-11 18:52:43 +0100

    new libs has been added

--- install_lib.sh
+++ install_lib.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # INSTALL PACKAGES
-PACKAGES="python-pip"
+# PACKAGES="python-pip"
 
-apt-get update
-apt-get upgrade -y
-apt-get install $PACKAGES -y
+# apt-get update
+# apt-get upgrade -y
+# apt-get install $PACKAGES -y
 pip install dash
+pip install Flask
+pip install w1thermsensor
