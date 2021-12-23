ede0d4a0cd4305b5af9920122e9d9616d4b29609
addussk <34662199+addussk@users.noreply.github.com>
2021-12-23 17:05:36 +0100

    updated

--- rp.py
+++ rp.py
@@ -12,23 +12,25 @@ if len(ports) == 0:
     exit(1)
 
 # For now we only expect one port to be available
-# host_port = ports[2]
+host_port = ports[0]
 
-# print("Found host port: {}".format(host_port.device))
 
-# ser = serial.Serial(
-#     host_port.device, 
-#     9600, 
-#     parity=serial.PARITY_NONE,
-#     stopbits=serial.STOPBITS_ONE,
-#     bytesize=serial.EIGHTBITS,
-#     timeout=1,
-#     write_timeout=1,
-# )
+print(host_port.device)
+print("Found host port: {}".format(host_port.device))
 
-# while True:
-#     print("writing")
-#     try:
-#         ser.write(b'hello')
-#     except:
-#         print("Write
+ser = serial.Serial(
+    host_port.device, 
+    9600, 
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1,
+    write_timeout=1,
+)
+
+while True:
+    print("writing")
+    try:
+        ser.write(b'hello')
+    except:
+        print("Write
