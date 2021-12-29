31cb3e8454825e4dc68a0b6bd06baefa7350aaaf
addussk <34662199+addussk@users.noreply.github.com>
2021-12-30 00:07:10 +0100

    clean up activities

--- scripts.py
+++ scripts.py
@@ -1,19 +1,6 @@
 import random, math
 from dashApp.models import FrontEndInfo
-import time
-from w1thermsensor import W1ThermSensor
 
-
-def dummy_temperature(min_temp=0, max_temp=100):
-    
-    sensor = W1ThermSensor()
-    temp = sensor.get_temperature()
-    print(temp)
-    return temp
-
-
-def dummy_val_fixed_meas(freq):
-    return abs(math.sin(freq))
 
 def dummy_val_tracking(freq, in_power, db):
     a, p = 1/10, 100
