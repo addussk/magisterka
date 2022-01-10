ebb35bf1869b5555380d4a128ad989b3a8377061
addussk <34662199+addussk@users.noreply.github.com>
2022-01-11 00:07:49 +0100

    bugfix-warnings related with sql alchemy has been resolved

--- dashApp/webapp.py
+++ dashApp/webapp.py
@@ -1,10 +1,15 @@
 import dash
 from flask_sqlalchemy import SQLAlchemy
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 def create_app(app):
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/test.db"
+    app.config['SECRET_KEY'] = 'asd asd asd'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
     db = SQLAlchemy(app)
     db.init_app(app)
     app.app_context().push()
@@ -16,9 +21,6 @@ def create_app(app):
     url_base_pathname='/dashboard/',
     suppress_callback_exceptions=True,
     )
-
-    app.server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/test.db"
-    app.server.config['SECRET_KEY'] = 'asd asd asd'
 
     from dashApp.layout import layout_main
     app.layout = layout_main
