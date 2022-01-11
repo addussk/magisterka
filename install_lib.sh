92716e944cfa612359022fee4d7d8463e1d37cdf
addussk <34662199+addussk@users.noreply.github.com>
2022-01-12 00:07:53 +0100

    install script has been updated, changed host address for different network

--- install_lib.sh
+++ install_lib.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 # INSTALL PACKAGES
-# PACKAGES="python-pip"
+sudo apt-get update -y
+sudo apt install git
+sudo apt install python3 idle3
+sudo apt-get install python3-pip
 
-# apt-get update
-# apt-get upgrade -y
-# apt-get install $PACKAGES -y
-pip install dash
-pip install Flask
-pip install w1thermsensor
-pip install board
-pip install adafruit-blinka
-pip install serial
-pip install Flask-SQLAlchemy
-pip install dash-daq
-pip install dash-extensions
-pip install pyserial
+#download repo 
+sudo git clone https://github.com/addussk/magisterka.git
+sudo python3 -m pip install dash
+sudo python3 -m pip install dash_daq
+sudo python3 -m pip install dash-extensions
+sudo python3 -m pip install Flask
+sudo python3 -m pip install Flask-SQLAlchemy
+sudo python3 -m pip install w1thermsensor
+sudo pip3 install adafruit-circuitpython-ads1x15
+sudo python3 -m pip install pyserial
+
+#configure ADC
+1) sudo raspi-config
+2) enable i2c intarface
+3) sudo i2cdetect -y 1 - to check whether i2c interface has been enabled properly
