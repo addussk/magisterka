c6d2f25e8170b89383d3fd1e1a76f7dc11bb9df7
addussk <34662199+addussk@users.noreply.github.com>
2022-05-25 23:24:42 +0200

    stara czesc zwiazana z diag window zostala usunieta z layoutu

--- dashApp/layout.py
+++ dashApp/layout.py
@@ -75,47 +75,7 @@ layout_main = html.Div(
             #n_intervals=50, # start at batch 50
             #disabled = True,
         ),
-        html.Div(
-            id="app-container",
-            children=[
-                build_tabs(),
-                # Main app
-                html.Div(id="app-content"),
-            ],
-        ),
-        html.Div(
-            id="dialogBox",
-            className="modal",
-            children=[
-                html.Div(
-                    id="dialog_container",
-                    className="modal-content",
-                    children=[                        
-                        html.Button("X", id="exit_btn", className="close"),
-                        html.H1("Are you confirm?"),
-                        html.Button("Confirm", id="confirm_btn", className="confirm"),
-                    ],
-                )
-            ],
-        ),
-        html.Div(
-            id="dialogBox_tab2",
-            className="modal",
-            children=[
-                html.Div(
-                    id="dialog_container_tab2",
-                    className="modal-content",
-                    children=[                        
-                        html.Button("X", id="exit_btn_tab2", className="close"),
-                        html.H1("Are you confirm?"),
-                        html.Button("Confirm", id="confirm_btn_tab2", className="confirm"),
-                    ],
-                )
-            ],
-        ),
-        html.Div([Keyboard(id="keyboard"), html.Div(id="output")]),
         html.Div(id="hide_part", children=[daq.BooleanSwitch(id='isDiagWindShow', on=False)],  style={'display':'none'}),
-        html.Div(id="hide_part_tab2", children=[daq.BooleanSwitch(id='isDiagWindShow_tab2', on=False)],  style={'display':'none'}),
         dcc.Store(id="value-setter-store", data=init_value_setter_store()),
     ],
 )
