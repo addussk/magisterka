15a7dd3e978ccd37dfaaf5a21ad400e8b26f3aa8
addussk <34662199+addussk@users.noreply.github.com>
2022-05-28 08:55:16 +0200

    usuniecie starych funkcji w  templates.py

--- dashApp/templates.py
+++ dashApp/templates.py
@@ -282,6 +282,50 @@ def build_set_panel():
         ]
     )
 
+def build_chart_panel():
+    return html.Div(
+        id="chart-panel",
+        children=[
+            html.Div(
+                id='chart-container',
+                children=[
+                    dcc.Graph(
+                        id="control-chart-live",
+                        figure=go.Figure(
+                            {
+                                "data": [
+                                    {
+                                        "x": [1,2,3,4,5],
+                                        "y": [1,2,3,4,5],
+                                        "mode": "lines+markers",
+                                    }
+                                ],
+                                "layout": {
+                                    "paper_bgcolor": "rgba(0,0,0,0)",
+                                    "plot_bgcolor": "rgba(0,0,0,0)",
+                                    "xaxis": dict(
+                                        showline=False, showgrid=False, zeroline=False
+                                    ),
+                                    "yaxis": dict(
+                                        showgrid=False, showline=False, zeroline=False
+                                    ),
+                                    "autosize": True,
+                                },
+                            }
+                        ),
+                    ),
+                ],
+            ),
+            html.Div(
+                id="stop-btn-container",
+                className="column",
+                children=[
+                    html.Button("Stop!", id="stop-btn", className="button",),
+                ]
+            ),
+        ],
+    )
+
 def build_value_setter_line(line_num, label, col3):
     row_style = {
         'color':'#c8f10f',
@@ -388,303 +432,3 @@ def fix_meas_tab(state_value):
             ],
         )
     ]
-    
-# Build header
-def generate_metric_row(id, style, col1, col2, col3):
-    if style is None:
-        style = {"height": "8rem", "width": "100%"}
-
-    return html.Div(
-        id=id,
-        className="row metric-row",
-        style=style,
-        children=[
-            html.Div(
-                id=col1["id"],
-                className="one column",
-                style={"margin-right": "2.5rem", "minWidth": "50px"},
-                children=col1["children"],
-            ),
-            html.Div(
-                id=col2["id"],
-                style={"textAlign": "center"},
-                className="two column",
-                children=col2["children"],
-            ),
-            html.Div(
-                id=col3["id"],
-                style={"height": "100%"},
-                className="four columns",
-                children=col3["children"],
-            ),
-        ],
-    )
-
-def generate_metric_list_header():
-    return generate_metric_row(
-        "metric_header",
-        {"height": "3rem", "margin": "1rem 0", "textAlign": "center"},
-        {"id": "m_header_1", "children": html.Div("Name")},
-        {"id": "m_header_2", "children": html.Div("Date")},
-        {"id": "m_header_3", "children": html.Div("Sparkline")},
-    )
-
-def generate_metric_row_helper(row_info):
-    x_axis = list()
-    y_axis = list()
-    results_table = Global_DataBase.read_filtered_table(row_info.get_time_scope())
-
-    for record in results_table:
-        x_axis.append(record.get_meas_pwr())
-        y_axis.append(len(x_axis))
-
-    return generate_metric_row (
-        ("row_" + str(row_info.get_id())),
-        None,
-        # kolumna z nazwa
-        {
-            "id": ("div_button_" + str(row_info.get_id())),
-            "className": "metric-row-button-text",
-            "children": html.Button(
-                id=(str(row_info.get_id()) + suffix_button_id),
-                className="metric-row-button",
-                children=row_info.get_name(),
-                # title pojawia sie po najechaniu na przycisk
-                title="Click to visualize live SPC chart",
-                n_clicks=0,
-            ),
-        },
-        # kolumna z data
-        {"id": (str(row_info.get_id()) + suffix_date), "children": (row_info.get_time_scope()[0].date())},
-        # kolumna z wykresem
-        {
-            "id": str(row_info.get_id()) + "_sparkline",
-            "children": dcc.Graph(
-                id=(str(row_info.get_id()) + suffix_sparkline_graph),
-                style={"width": "100%", "height": "95%"},
-                config={
-                    "staticPlot": False,
-                    "editable": False,
-                    "displayModeBar": False,
-                },
-                figure=go.Figure(
-                    {
-                        "data": [
-                            {
-                                "x": y_axis,
-                                "y": x_axis,
-                                "mode": "lines+markers",
-                                "name": row_info.get_name(),
-                                "line": {"color": "#f4d44d"},
-                            }
-                        ],
-                        "layout": {
-                            "uirevision": True,
-                            "margin": dict(l=0, r=0, t=4, b=4, pad=0),
-                            "xaxis": dict(
-                                showline=False,
-                                showgrid=False,
-                                zeroline=False,
-                                showticklabels=False,
-                            ),
-                            "yaxis": dict(
-                                showline=False,
-                                showgrid=False,
-                                zeroline=False,
-                                showticklabels=False,
-                            ),
-                            "paper_bgcolor": "rgba(0,0,0,0)",
-                            "plot_bgcolor": "rgba(0,0,0,0)",
-                        },
-                    }
-                ),
-            ),
-        },)
-
-def build_tabs():
-    return html.Div(
-        id="tabs",
-        className="tabs",
-        children=[
-            dcc.Tabs(
-                id="app-tabs",
-                value="tab2",
-                className="custom-tabs",
-                children=[
-                    dcc.Tab(
-                        id="Specs-tab",
-                        label="Measurements Settings",
-                        value="tab1",
-                        className="custom-tab",
-                        selected_className="custom-tab--selected",
-                    ),
-                    dcc.Tab(
-                        id="Control-chart-tab",
-                        label="Live Chart Measurements",
-                        value="tab2",
-                        className="custom-tab",
-                        selected_className="custom-tab--selected",
-                    ),
-                ],
-            )
-        ],
-    )
-
-def generate_section_banner(title):
-    return html.Div(className="section-banner", children=title)
-
-def build_value_setter_line(line_num, label, col3):
-    row_style = {
-        'color':'#c8f10f',
-        'fontSize': '1.5rem',
-    }
-
-    return html.Div(
-        id=line_num,
-        children=[
-            html.Label(label, className='first_diag_col left'),
-            html.Div(col3, className='sec_diag_col left'),
-        ],
-        className="row-diag-window",
-        style=row_style
-    )
-    
-def build_tab_1():
-    return [
-        # Zawartosc tab 1
-        html.Div(
-            id="settings-menu",
-            children=[
-                # Kolumna po lewej 7/12 szer, panel zawierajacy elementy z nastawa urzadzen
-                html.Div(
-                    id="meas-sett-panel",
-                    className="seven columns",
-                    children=[
-                        # Div zawierajacy drop down list do wyboru trybu pomiaru
-                        html.Div(
-                            id="metric-select-menu",
-                            # className='five columns',
-                            children=[
-                                html.Label(id="metric-select-title", children="Select Measurement Mode"),
-                                html.Br(),
-                                dcc.Dropdown(
-                                    id="metric-select-dropdown",
-                                    options=list(
-                                        {"label": mode, "value": DROP_LIST_MEAS_MODE[mode]} for mode in DROP_LIST_MEAS_MODE
-                                    ),
-                                    value=0,
-                                ),
-                            ],
-                        ),
-                        # Div zawierajacy elementy do ustawiania parametrow dla wybranego trybu, tworzone dynamicznie za pomoca callbackow
-                        html.Div(
-                            id="value-setter-menu",
-                            # className='six columns',
-                            children=[
-                                html.Div(id="value-setter-panel"),
-                                html.Br(),
-                                html.Div(
-                                    id="button-div",
-                                    children=[
-                                        html.Button("Set new setup", id="value-setter-set-btn"),
-                                        html.Button(
-                                            "Stop measurement",
-                                            id="value-setter-view-btn",
-                                        ),
-                                    ]
-                                ),
-                                html.Div(
-                                    id="value-setter-view-output", className="output-datatable"
-                                ),
-                            ]
-                        )
-                    
-                    ]
-                ),
-            ],
-        ),
-    ]
-
-def build_chart_panel():
-    return html.Div(
-        id="chart-panel",
-        children=[
-            html.Div(
-                id='chart-container',
-                children=[
-                    dcc.Graph(
-                        id="control-chart-live",
-                        figure=go.Figure(
-                            {
-                                "data": [
-                                    {
-                                        "x": [1,2,3,4,5],
-                                        "y": [1,2,3,4,5],
-                                        "mode": "lines+markers",
-                                    }
-                                ],
-                                "layout": {
-                                    "paper_bgcolor": "rgba(0,0,0,0)",
-                                    "plot_bgcolor": "rgba(0,0,0,0)",
-                                    "xaxis": dict(
-                                        showline=False, showgrid=False, zeroline=False
-                                    ),
-                                    "yaxis": dict(
-                                        showgrid=False, showline=False, zeroline=False
-                                    ),
-                                    "autosize": True,
-                                },
-                            }
-                        ),
-                    ),
-                ],
-            ),
-            html.Div(
-                id="stop-btn-container",
-                className="column",
-                children=[
-                    html.Button("Stop!", id="stop-btn", className="button",),
-                ]
-            ),
-        ],
-    )
-
-def build_bottom_panel():
-    return html.Div(
-        id="top-section-container",
-        className="row",
-        children=[
-            # Table with all chapters
-            html.Div(
-                id="metric-summary-session",
-                className="eight columns",
-                children=[
-                    generate_section_banner("Measurements list: "),
-                    html.Div(
-                        id="metric-div",
-                        children=[
-                            generate_metric_list_header(),
-                            html.Div(
-                                id="metric-rows",
-                                children=[
-                                    generate_metric_row_helper(el) for el in Global_DataBase.read_record_all(MeasurementInfo)
-                                ],
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            # Temperature indicator
-            html.Div(
-                id="temp-indicator",
-                className="four columns",
-                children=[
-                    generate_section_banner("Current temperature (Celsius)"),
-                    generate_indicator(),
-                ],
-            ),
-        ],
-    )
-
-
-
